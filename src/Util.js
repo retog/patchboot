bf5437d6314bb20bd8dcfd0dd78545e534d94506
Reto Gmür <reto@factsmission.com>
2021-04-20 14:06:55 +0200

    simpler Utf8ArrayToStr

--- src/Util.js
+++ src/Util.js
@@ -9,45 +9,25 @@ export default class Util {
 
   getBlob(blobId) {
     function Utf8ArrayToStr(array) {
-      var out, i, len, c;
-      var char2, char3;
-
-      out = "";
-      len = array.length;
-      i = 0;
-      while (i < len) {
-        c = array[i++];
-        switch (c >> 4) {
-          case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-            // 0xxxxxxx
-            out += String.fromCharCode(c);
-            break;
-          case 12: case 13:
-            // 110x xxxx   10xx xxxx
-            char2 = array[i++];
-            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-            break;
-          case 14:
-            // 1110 xxxx  10xx xxxx  10xx xxxx
-            char2 = array[i++];
-            char3 = array[i++];
-            out += String.fromCharCode(((c & 0x0F) << 12) |
-              ((char2 & 0x3F) << 6) |
-              ((char3 & 0x3F) << 0));
-            break;
-        }
-      }
-
-      return out;
+      return new Promise((resolve, reject) => {
+        var bb = new Blob([array]);
+        var f = new FileReader();
+        f.onload = function(e) {
+            resolve(e.target.result);
+        };
+        f.readAsText(bb);
+      })
     }
+
+    
+
     return new Promise((resolve, reject) => {
       this.sbot.blobs.want(blobId).then(() => {
         pull(
           this.sbot.blobs.get(blobId),
           pull.collect((err, values) => {
             if (err) reject(err)
-            const code = values.map(v => Utf8ArrayToStr(v)).join('')
-            resolve(code)
+            Promise.all(values.map(v => Utf8ArrayToStr(v))).then(s => s.join('')).then(resolve)
           })
         )
       })
